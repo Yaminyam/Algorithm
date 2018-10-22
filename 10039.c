30ebc1ace201e46849774ffe857350075fb8c338
Yami <siontama@naver.com>
2018-10-22 22:57:04 +0900

    평균점수

---
+++ 10039.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	int n;
+	int sum = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		scanf("%d", &n);
+		if (n < 40)sum += 40;
+		else sum += n;
+	}
+	printf("%d", sum / 5);
+}
