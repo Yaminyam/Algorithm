148bd13b4334da1b3d7ff9088a916c4508862290
Yami <31057849+siontama@users.noreply.github.com>
2019-01-16 23:59:53 +0900

    2×n 타일링 2

---
+++ BOJ/11727.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int d[1001];
+	int n;
+	scanf("%d", &n);
+	d[0] = 1;
+	d[1] = 1;
+	for (int i = 2; i <= n; i++)
+	{
+		d[i] = d[i - 1] + 2*d[i - 2];
+		d[i] %= 10007;
+	}
+	printf("%d", d[n]);
+}
