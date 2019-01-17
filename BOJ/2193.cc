da9bf57cdcb8d38b3e6aeff27b7a988a9173404e
Yami <31057849+siontama@users.noreply.github.com>
2019-01-17 19:22:07 +0900

    이친수

---
+++ BOJ/2193.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+long long d[91];
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	d[0] = 0;
+	d[1] = 1;
+	for (int i = 2; i <= n; i++)
+	{
+		d[i] = d[i - 1] + d[i - 2];
+	}
+	cout << d[n] << endl;
+}
