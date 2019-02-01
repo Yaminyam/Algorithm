5e4d17a9a299397e06135c7b4cb5672b9013387b
Yami <31057849+siontama@users.noreply.github.com>
2019-02-01 23:32:00 +0900

    A+B - 7

---
+++ BOJ/11021.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+	int t;
+	int a, b;
+	scanf("%d", &t);
+	int count = 0;
+	while (t--) {
+		scanf("%d %d", &a, &b);
+		printf("Case #%d: %d\n", ++count, a + b);
+	}
+}
