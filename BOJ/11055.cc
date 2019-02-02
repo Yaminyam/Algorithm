55ebaf54cb0e9853c1d0cb50dd8852932a2fae13
Yami <31057849+siontama@users.noreply.github.com>
2019-02-02 22:38:31 +0900

    가장 큰 증가 부분 수열

---
+++ BOJ/11055.cc
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+	int d[1000];
+	int a[1000];
+	int n;
+	int result = 0;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+	for (int i = 0; i < n; i++) {
+		d[i] = a[i];
+		for (int j = 0; j < i; j++) {
+			if (a[j] < a[i] && d[i] < d[j] + a[i]) {
+				d[i] = d[j] + a[i];
+			}
+		}
+		result = max(result, d[i]);
+	}
+	printf("%d", result);
+}
