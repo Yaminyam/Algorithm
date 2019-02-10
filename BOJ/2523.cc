d9d22b7457ede8ab07ed583b9daab6f1fbc055a7
Yami <31057849+siontama@users.noreply.github.com>
2019-02-10 21:04:21 +0900

    별 찍기 - 13

---
+++ BOJ/2523.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j < i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (int i = n-1; i > 0; i--) {
+		for (int j = 0; j < i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+}
