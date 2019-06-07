df32e52937c647bc509f0535d7ebf556b042b180
siontama <siontama@gmail.com>
2019-06-08 08:39:34 +0900

    수 정렬하기 5

---
+++ BOJ/15688.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<algorithm>
+#include<functional>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	int input[1000000];
+	cin >> n;
+	for (int i = 0; i < n; i++)scanf("%d", &input[i]);
+	sort(input, input + n);
+	for (int i = 0; i < n; i++)printf("%d\n", input[i]);
+}
