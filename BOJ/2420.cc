aac0f392c75499b38d27c8e5665c762d705c4057
siontama <siontama@gmail.com>
2019-06-10 20:19:46 +0900

    사파리 월드

---
+++ BOJ/2420.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+	long long n, m;
+	scanf("%lld %lld", &n, &m);
+	printf("%lld", abs(n - m));
+	
+}
