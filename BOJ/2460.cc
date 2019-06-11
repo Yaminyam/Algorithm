caf23cb89653f527dacf32be5392ae18694599f0
siontama <siontama@gmail.com>
2019-06-11 21:13:02 +0900

    지능형 기차 2

---
+++ BOJ/2460.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+	int input[10][2];
+	int result = 0;
+	int sum = 0;
+	for (int i = 0; i < 10; i++) {
+		cin >> input[i][0] >> input[i][1];
+		sum -= input[i][0];
+		sum += input[i][1];
+		if (sum > result)result = sum;
+	}
+	printf("%d", result);
+}
