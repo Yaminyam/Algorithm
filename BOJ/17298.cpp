b79654d614be2d9b786bb0bf1318845b94165497
Yami <31057849+siontama@users.noreply.github.com>
2020-03-18 21:19:01 +0900

    오큰수

--- BOJ/17298.cpp
+++ BOJ/17298.cpp
@@ -5,29 +5,26 @@ using namespace std;
 
 int main()
 {
-    stack<int> st;
-    int n;
-    int arr[1000000];
-    int ans[1000000];
-    scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
-    }
-    for(int i=0;i<n;i++){
-        if(st.empty()){
-            st.push(i);
-        }
-        if(!st.empty() && arr[st.top()]<arr[i]){
-            ans[st.top()] = arr[i];
-            st.pop();
-        }
-        st.push(i);
-    }
-    while(!st.empty()){
-        ans[st.top()] = -1;
-        st.pop();
-    }
-    for(int i=0;i<n;i++){
-        cout << ans[i] << ' ';
-    }
+	stack<int> st;
+	int n;
+	int arr[1000000];
+	int ans[1000000];
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+	for (int i = 0; i < n; i++) {
+		while (!st.empty() && arr[st.top()] < arr[i]) {
+			ans[st.top()] = arr[i];
+			st.pop();
+		}
+		st.push(i);
+	}
+	while (!st.empty()) {
+		ans[st.top()] = -1;
+		st.pop();
+	}
+	for (int i = 0; i < n; i++) {
+		cout << ans[i] << ' ';
+	}
 }
