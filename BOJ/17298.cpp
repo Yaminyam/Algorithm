244fd83fbb19f97ef280619942d5e247ce5a97d5
Yami <31057849+siontama@users.noreply.github.com>
2020-03-18 21:13:54 +0900

    오큰수

---
+++ BOJ/17298.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    stack<int> st;
+    int n;
+    int arr[1000000];
+    int ans[1000000];
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        if(st.empty()){
+            st.push(i);
+        }
+        if(!st.empty() && arr[st.top()]<arr[i]){
+            ans[st.top()] = arr[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+    while(!st.empty()){
+        ans[st.top()] = -1;
+        st.pop();
+    }
+    for(int i=0;i<n;i++){
+        cout << ans[i] << ' ';
+    }
+}
