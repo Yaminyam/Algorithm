b9b2b4d9767853cb6548cc9fbf721950737633c7
Yami <31057849+siontama@users.noreply.github.com>
2020-07-16 15:37:18 +0900

    연세대학교

---
+++ BOJ/15680.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+	int input;
+	cin >> input;
+	if (input == 0) {
+		cout << "YONSEI";
+	}
+	else if (input == 1) {
+		cout << "Leading the Way to the Future";
+	}
+}
+
+
