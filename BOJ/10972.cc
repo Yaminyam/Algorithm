7b866adb62439a4fa93dd6efadfe1a7421bb2076
Yami <31057849+siontama@users.noreply.github.com>
2020-08-14 02:14:44 +0900

    다음 순열

---
+++ BOJ/10972.cc
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+vector<int> arr;
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		arr.push_back(x);
+	}
+	if (next_permutation(arr.begin(), arr.end())) {
+		for (int i = 0; i < n; i++) {
+			cout << arr[i] << " ";
+		}
+	}
+	else {
+		cout << "-1";
+	}
+}
