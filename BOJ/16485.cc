0a53813a3423d9fac785f2275a2932d4e733084b
Yami <31057849+siontama@users.noreply.github.com>
2021-01-02 20:09:46 +0900

    작도하자! - ②

---
+++ BOJ/16485.cc
@@ -0,0 +1,24 @@
+#define _USE_MATH_DEFINES
+
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+
+#include<algorithm>
+#include<functional>
+
+#include<string>
+#include<vector>
+#include<queue>
+#include<stack>
+
+using namespace std;
+const int INF = numeric_limits<int>::max();
+const double pi = M_PI;
+
+int main()
+{
+	double c, b;
+	cin >> c >> b;
+	printf("%.7lf", c / b);
+}
