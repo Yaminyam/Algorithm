f3efc1248e9c0f8ffdcdad9c56c704425c866b16
Yami <31057849+siontama@users.noreply.github.com>
2021-01-03 20:40:42 +0900

    피카츄가 낸 어려운 문제

---
+++ BOJ/16488.cc
@@ -0,0 +1,24 @@
+#define _USE_MATH_DEFINES
+
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+
+#include<algorithm>
+#include<functional>
+
+#include<string>
+#include<vector>
+#include<queue>
+#include<stack>
+
+using namespace std;
+const int INF = numeric_limits<int>::max();
+const double pi = M_PI;
+
+int main()
+{
+	long long n, k;
+	cin >> n >> k;
+	cout << n * n * k;
+}
