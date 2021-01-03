f546cca72eb263a8a6e1ccab5609053ff35812d8
Yami <31057849+siontama@users.noreply.github.com>
2021-01-03 20:40:58 +0900

    외계인의 침투

---
+++ BOJ/16490.cc
@@ -0,0 +1,24 @@
+#define _USE_MATH_DEFINES
+
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+
+#include<algorithm>
+#include<functional>
+
+#include<string>
+#include<vector>
+#include<queue>
+#include<stack>
+
+using namespace std;
+const int INF = numeric_limits<int>::max();
+const double pi = M_PI;
+
+int main()
+{
+	double a, t;
+	cin >> a >> t;
+	cout << a * a - t * t;
+}
