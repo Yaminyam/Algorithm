ddf136ee9efe7726f8fca688fafc2abe9fb40de5
Yami <31057849+siontama@users.noreply.github.com>
2021-01-11 01:52:03 +0900

    랜덤 게임~~~~~

---
+++ BOJ/19617.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+
+#include <algorithm>
+#include <functional>
+
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+
+using namespace std;
+const int INF = numeric_limits<int>::max();
+
+int main()
+{
+	
+	cout << INF/2;
+}
