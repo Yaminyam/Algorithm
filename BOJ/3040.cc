75c767114fb20d4fc9563c93962d0c00a51b9f25
Yami <31057849+siontama@users.noreply.github.com>
2021-01-30 22:45:37 +0900

    백설 공주와 일곱 난쟁이

---
+++ BOJ/3040.cc
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int sum = 0;
+int mans[9];
+
+void func() {
+	for (int i = 0; i < 9; i++) {
+		for (int j = i + 1; j < 9; j++) {
+			if (sum - mans[i] - mans[j] == 100) {
+				mans[i] = -1;
+				mans[j] = -1;
+				return;
+			}
+		}
+	}
+}
+
+int main()
+{
+
+	for (int i = 0; i < 9; i++) {
+		cin >> mans[i];
+	}
+
+	for (int i = 0; i < 9; i++) {
+		sum += mans[i];
+	}
+
+	func();
+	sort(mans, mans + 9);
+
+	for (int i = 2; i < 9; i++) {
+		cout << mans[i] << "\n";
+	}
+}
