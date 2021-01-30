293cd4bdbc9021690f867678a078841602f517a2
Yami <31057849+siontama@users.noreply.github.com>
2021-01-30 22:46:09 +0900

    직각삼각형

---
+++ BOJ/4153.cc
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    while (1)
+    {
+        int a[3];
+        scanf("%d %d %d", &a[0], &a[1], &a[2]);
+        sort(a,a+3);
+        if (a[0] == 0 && a[1] == 0 && a[2] == 0)
+        {
+            break;
+        }
+        if(a[0]*a[0] + a[1]*a[1] == a[2]*a[2])
+            printf("right\n");
+        else
+        {
+            printf("wrong\n");
+        }
+        
+    }
+}
