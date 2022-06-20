c32853462e0546c28b8b4e9c41047b57ac815263
siontama <siontama@naver.com>
2022-06-20 14:34:19 +0900

    배열 합치기

--- BOJ/11728.cc
+++ BOJ/11728.cc
@@ -1,20 +1,61 @@
-#include<iostream>
-#include<string>
-#include<algorithm>
-#include<vector>
-
-using namespace std;
-
+#include<stdio.h>
+int partition(int left, int right, int a);
+void swap1(int i, int j, int a[]);
+void quicksort(int a[], int i, int j);
 int main()
 {
-	int n, m;
-	int input[2000001];
-	cin >> n >> m;
-	for (int i = 0; i < n + m; i++) {
-		cin >> input[i];
+	int A, B;
+	scanf("%d %d", &A, &B);
+	int a[2000000];
+	int j, i;
+	i = 0;
+	j = A + B - 1;
+	for (int k = 0; k < A + B; k++)
+	{
+		scanf("%d", &a[k]);
 	}
-	sort(input, input + n + m);
-	for (int i = 0; i < n + m; i++) {
-		cout << input[i] << " ";
+	quicksort(a, i, j);
+	for (int k = 0; k < A + B; k++)
+	{
+		printf("%d ", a[k]);
 	}
+	return 0;
+}
+void swap1(int i, int j, int a[])
+{
+	int temp;
+	temp = a[i];
+	a[i] = a[j];
+	a[j] = temp;
+}
+int partition(int left, int right, int a[])
+{
+	int i, j, pivot;
+	i = left;
+	j = right;
+	pivot = a[left];
+	while (i < j)
+	{
+		while (pivot < a[j]) {
+			j--;
+		}
+
+		while (i < j && pivot >= a[i]) {
+			i++;
+		}
+		swap1(i, j, a);
+	}
+
+	swap1(left, i, a);
+	return j;
+}
+void quicksort(int a[], int left, int right)
+{
+	if (left >= right)
+	{
+		return;
+	}
+	int p = partition(left, right, a);
+	quicksort(a, left, p - 1);
+	quicksort(a, p + 1, right);
 }
